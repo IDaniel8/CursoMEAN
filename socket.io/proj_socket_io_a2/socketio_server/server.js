06d760fb35af8a50032159fea7041174ee5fd2ed
idaniel <alidanft@gmail.com>
2017-05-03 13:01:41 +0200

    socket.io refactorizado

---
+++ socket.io/proj_socket_io_a2/socketio_server/server.js
@@ -0,0 +1,29 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+var sockets = [];
+io.origins("http://localhost:4200");
+var chat = io.of("/chat");
+
+chat.on("connection", (socket) => {
+    console.log("Cliente conectado!!");
+    sockets.push(socket);
+
+    socket.on("mando-un-mensaje", (mensaje) => {
+        console.log("Mensaje recibido: ", mensaje);
+
+        mensaje.user = socket.id;
+
+        socket.emit("mando-un-mensaje", mensaje);
+        //chat.emit("mando-un-mensaje", mensaje);
+        socket.broadcast.emit("mando-un-mensaje", mensaje);
+    })
+});
+chat.on("disconnect", (socket) => {
+    console.log("Cliente desconectado!!");
+});
+
+http.listen(3000, () => {
+    console.log("Servidor iniciado *:3000");
+})
