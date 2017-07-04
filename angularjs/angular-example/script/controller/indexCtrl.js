326964772ea1ee31cea32a9437a7c70a394538f9
Daniel Febres <daniel.febres@bclubber.com>
2017-07-04 18:17:31 +0200

    changelog angularjs 0.0.5.1

--- angularjs/angular-example/script/controller/indexCtrl.js
+++ angularjs/angular-example/script/controller/indexCtrl.js
@@ -1,14 +1,23 @@
 'use strict';
-angular.module('ejemploApp').controller('indexCtrl', ['$scope', function($scope){
-
+angular.module('ejemploApp').controller('scopeCtrl', ['$scope', function($scope){
     $scope.nombre = 'John Doe';
-
     $scope.saludar = function() {
         alert('Hola, buenas noches!');
     }
-
     $scope.contar = function() {
         alert('1,2,3....');
     }
+}]);
+
+angular.module('ejemploApp').controller('watchCtrl', ['$scope', function($scope){
+    
+    $scope.$watch(
+        function() {
+            return $scope.valorCheckbox;
+        },
+        function(valorNuevo, valorAntiguo) {
+            alert(`valor nuevo: ${valorNuevo} || valor antiguo ${valorAntiguo}`);
+        }
+    );
 
 }]);
